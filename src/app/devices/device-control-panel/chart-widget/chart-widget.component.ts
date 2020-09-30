af76ec1136bdafe46e84f801f54c638d94d02c74
bhakti-kemsys <bhaktigandhi@kemsys.com>
2020-09-30 19:07:06 +0530

    Fixed Y1 axis error issue

--- src/app/devices/device-control-panel/chart-widget/chart-widget.component.ts
+++ src/app/devices/device-control-panel/chart-widget/chart-widget.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
-import { GoogleChartInterface } from 'ng2-google-charts';
+import { GoogleChartInterface, GoogleChartComponent } from 'ng2-google-charts';
 import { log } from 'console';
+import { Device } from 'src/app/models/device.model';
+import { Subscription } from 'rxjs';
+import { CommonService } from 'src/app/services/common.service';
+import { DeviceService } from 'src/app/services/devices/device.service';
+import { ToasterService } from 'src/app/services/toaster.service';
 
 declare var $: any
 @Component({
@@ -21,7 +26,7 @@ export class ChartWidgetComponent implements OnInit {
   busesOnDuty = []
   maintenance = []
   title = 'Bus Statistics';
-  type = 'ColumnChart';
+  type = '';
   columnNames = []
   options = { isStacked: true };
   width = 650
@@ -31,9 +36,25 @@ export class ChartWidgetComponent implements OnInit {
   chartIcons = ["", "fa-bar-chart", "fa-line-chart", "fa-area-chart", "fa-pie-chart", "", ""]
   public selectedChartType = ""
   widgetTitle = ""
-  isCollapsed : boolean = false
+  isCollapsed: boolean = false
   public chartDataTable: GoogleChartInterface;
-  public showDataTable : boolean = false
+  public showDataTable: boolean = false
+  isLoading: boolean = false
+
+  historyFilter: any = {};
+  apiSubscriptions: Subscription[] = [];
+  historyData: any[] = [];
+  device = new Device();
+  isLayout = false;
+  y1AxisProps = [];
+  y2AxisProp = [];
+  // y1AxisProps = "";
+  // y2AxisProp = "";
+  xAxisProps = "";
+  isFilterSelected = false;
+  currentLayout
+  showError: boolean = false
+  errorMsg = "";
 
   // google chart
   public chartData: GoogleChartInterface = {  // use :any or :GoogleChartInterface
@@ -55,8 +76,8 @@ export class ChartWidgetComponent implements OnInit {
       series: {
       },
       vAxes: {
-          // Adds titles to each axis.
-        },
+        // Adds titles to each axis.
+      },
       height: 400,
       // width: 350,
       curveType: 'function',
@@ -64,113 +85,289 @@ export class ChartWidgetComponent implements OnInit {
         actions: ['dragToZoom', 'rightClickToReset'],
         axis: 'horizontal',
         keepInBounds: true,
-        maxZoomIn: 10.0}
+        maxZoomIn: 10.0
+      }
     }
   };
-  constructor() { }
+  constructor(private deviceService: DeviceService,
+    private commonService: CommonService,
+    private toasterService: ToasterService) { }
 
   ngOnInit(): void {
-    if(this.showDataTable){
-      this.chartDataTable = {...this.chartData}
-      this.chartDataTable.chartType = "Table"
+    if ( Object.keys(this.currentLayout).length === 0) {
+      this.currentLayout = null
+      if (this.type.indexOf('Pie') <= -1) {
+        if (!this.y1AxisProps || (this.y1AxisProps && this.y1AxisProps.length === 0)) {
+          this.showError = true
+          this.errorMsg = 'Y1 Axis Property is required'
+          this.toasterService.showError('Y1 Axis Property is required', 'Load Chart');
+          return
+        }
+      }
+      else {
+        if (this.xAxisProps.length == 0) {
+          this.showError = true
+          this.errorMsg = 'X Axis Property is required'
+          this.toasterService.showError('X Axis Property is required', 'Load Chart');
+          return
+        }
+      }
     }
+    else {
+      console.log('this.currentLayout ',this.currentLayout)
+      if (this.currentLayout.chartType.indexOf('Pie') <= -1) {
+        if (!this.currentLayout.y1axis || (this.currentLayout.y1axis && this.currentLayout.y1axis.length === 0)) {
+          this.showError = true
+          this.errorMsg = 'Y1 Axis Property is required'
+          this.toasterService.showError('Y1 Axis Property is required', 'Load Chart');
+          return
+        }
+      }
+      else {
+        if (this.currentLayout.xAxis.length == 0) {
+          this.showError = true
+          this.errorMsg = 'X Axis Property is required'
+          this.toasterService.showError('X Axis Property is required', 'Load Chart');
+          return
+        }
+      }
+    }
+    this.searchHistory(this.currentLayout).then((plottedChart: GoogleChartInterface) => {
+      // const ccComponent = this.chartData.component;
+      // ccComponent.draw();
+      this.chartData = {...plottedChart}
+      let showTable = this.currentLayout ? this.currentLayout.showDataTable : this.showDataTable
+      if (showTable) {
+        this.chartData.options.width = 500
+      }
+      else {
+        this.chartData.options.width = 1000
+      }
+      if (this.showDataTable) {
+        this.chartDataTable = { ...this.chartData }
+        this.chartDataTable.chartType = "Table"
+      }
+      // console.log('this.chartData ',this.chartData)
+      if (this.chartData.chartType == "PieChart" || this.chartData.chartType == "Pie Chart with table") {
+        delete this.chartData.options.explorer
+        delete this.chartData.options.legend
+        if (this.chartData.chartType == "Pie Chart with table") {
+          this.showDataTable = true
+          this.chartData.chartType = "PieChart"
+        }
+      }
+    })
   }
 
   ngAfterViewInit() {
-    $("#" + this.chartId).draggable({ handle: ".card-header",containment: "#widgetContainer" })
+    if(this.isLayout){
+      $("#" + this.chartId).draggable({ handle: ".card-header", containment: "#layoutWidgetContainer" })
       .resizable();
-    $("#" + this.chartId).on('resize',(event)=>{
-      if(parseInt(event.target.style.width.split('px')[0])>700){
-        $("#"+this.chartId).parent().parent().addClass('col-lg-12').removeClass('col-lg-6')
+    }
+    else{
+      $("#" + this.chartId).draggable({ handle: ".card-header", containment: "#widgetContainer" })
+        .resizable();
+    }
+    $("#" + this.chartId).on('resize', (event) => {
+      if (parseInt(event.target.style.width.split('px')[0]) > 700) {
+        $("#" + this.chartId).parent().parent().addClass('col-lg-12').removeClass('col-lg-6')
       }
-      else{
-        $("#"+this.chartId).parent().parent().addClass('col-lg-6').removeClass('col-lg-12')
+      else {
+        $("#" + this.chartId).parent().parent().addClass('col-lg-6').removeClass('col-lg-12')
       }
-      
+
     })
-    // $("#collapse_" + this.chartId).on("click", () => {
-    //   if ($("#box_"+this.chartId).hasClass('collapsed-box')) { 
-    //     $("#box_"+this.chartId).removeClass('collapsed-box') 
-    //     this.isCollapsed = false
-    //   } else {
-    //      $("#box_"+this.chartId).addClass('collapsed-box') 
-    //      this.isCollapsed = true
-    //     } 
-    // })
     $("#remove_" + this.chartId).on("click", () => {
+      console.log('remove')
       //immediate parent is the component tag and it's parent is the col div
-      $("#"+this.chartId).parent().parent().remove()
+      $("#" + this.chartId).parent().parent().remove()
     })
   }
 
-  loadChart() {
-    let today = moment()
-    for (var i = 0; i < 5; i++) {
-      this.dates.push(today.subtract(i, 'd').format("MM-DD-YYYY"))
-      today = moment()
-    }
-    this.dates = this.dates.reverse()
-    this.busesOnDuty = []
-    this.maintenance = []
-    // this.columnNames = []
-    let loopCnt = 5
-    // if (this.y1axis == 'month') {
-    //   loopCnt = 12
-    // }
-    // $.each($("input[name='yaxis']:checked"), ((ele) => {
-    //   this.columnNames.push(ele);
-    // }))
-    // if (this.columnNames.length > 0) {
-      for (let index = 0; index < loopCnt; index++) {
-        if (this.columnNames.length == 1) {
-          if (this.y1axis.length==1) {
-            // this.busesOnDuty.push([this.dates[index],Math.floor((Math.random() * 100) + 1),Math.floor((Math.random() * 100) + 1)])
-            this.busesOnDuty.push([this.dates[index], Math.floor((Math.random() * 100) + 1)])
-          }
-          else if (this.y1axis.length==2) {
-            // this.busesOnDuty.push([this.year[index],Math.floor((Math.random() * 100) + 1),Math.floor((Math.random() * 100) + 1)])
-            this.busesOnDuty.push([this.year[index], Math.floor((Math.random() * 100) + 1)])
-          }
-          else {
-            // this.busesOnDuty.push([this.months[index],Math.floor((Math.random() * 100) + 1),Math.floor((Math.random() * 100) + 1)])
-            this.busesOnDuty.push([this.months[index], Math.floor((Math.random() * 100) + 1)])
-          }
-        }
-        else {
-
-          if (this.y1axis.length==1) {
-            this.busesOnDuty.push([this.dates[index], Math.floor((Math.random() * 100) + 1), Math.floor((Math.random() * 100) + 1)])
-            // this.busesOnDuty.push([this.dates[index],Math.floor((Math.random() * 100) + 1)])
-          }
-          else if (this.y1axis.length==2) {
-            this.busesOnDuty.push([this.year[index], Math.floor((Math.random() * 100) + 1), Math.floor((Math.random() * 100) + 1)])
-            // this.busesOnDuty.push([this.year[index],Math.floor((Math.random() * 100) + 1)])
-          }
-          else {
-            this.busesOnDuty.push([this.months[index], Math.floor((Math.random() * 100) + 1), Math.floor((Math.random() * 100) + 1)])
-            // this.busesOnDuty.push([this.year[index],Math.floor((Math.random() * 100) + 1)])
-          }
+  searchHistory(layoutJson) {
+    let lineGoogleChartData: GoogleChartInterface = {  // use :any or :GoogleChartInterface
+      chartType: '',
+      dataTable: [],
+      options: {
+        interpolateNulls: true,
+        hAxis: {
+          viewWindowMode: 'pretty',
+          slantedText: true,
+          textStyle: {
+            fontSize: 10
+          },
+          slantedTextAngle: 60
+        },
+        legend: {
+          position: 'top'
+        },
+        series: {
+        },
+        vAxes: {
+          // Adds titles to each axis.
+        },
+        height: 300,
+        width: 600,
+        curveType: 'function',
+        explorer: {
+          actions: ['dragToZoom', 'rightClickToReset'],
+          axis: 'horizontal',
+          keepInBounds: true,
+          maxZoomIn: 10.0
         }
       }
-    // }
-    // else {
-    //   alert("Please select Y-axis parameter")
-    // }
-  }
-
-  changeChartType(selectedChartType) {
-    if (selectedChartType == "StackedBarChart") {
-      this.type = "ColumnChart"
-      this.options['isStacked'] = true
-    }
-    else {
-
-      if (selectedChartType == "PieChart" && this.columnNames.length == 2) {
-        alert("Pie chart does not support 2D values on X-axis")
+    };
+    return new Promise((resolve, reject) => {
+      let currentHistoryFilter = { ...this.historyFilter }
+      currentHistoryFilter.y1AxisProperty = []
+      currentHistoryFilter.y2AxisProperty = []
+      currentHistoryFilter.xAxisProps = this.xAxisProps
+      if (layoutJson == null) {
+        currentHistoryFilter.y1AxisProperty = this.y1AxisProps
+        currentHistoryFilter.y2AxisProperty = this.y2AxisProp
+      }
+      else {
+        currentHistoryFilter.y1AxisProperty = layoutJson.y1axis
+        currentHistoryFilter.y2AxisProperty = layoutJson.y2axis
+        currentHistoryFilter.xAxisProps = layoutJson.xAxis
+      }
+      currentHistoryFilter.to_date = this.historyFilter.to_date
+      currentHistoryFilter.from_date = this.historyFilter.from_date
+      this.isLoading = true;
+      lineGoogleChartData.dataTable = [];
+      const obj = { ...currentHistoryFilter };
+      const now = moment().utc();
+      obj.to_date = now.unix();
+      if (this.historyFilter.dateOption === '5 mins') {
+        obj.from_date = (now.subtract(5, 'minute')).unix();
+      } else if (this.historyFilter.dateOption === '30 mins') {
+        obj.from_date = (now.subtract(30, 'minute')).unix();
+      } else if (this.historyFilter.dateOption === '1 hour') {
+        obj.from_date = (now.subtract(1, 'hour')).unix();
+      } else if (this.historyFilter.dateOption === '24 hour') {
+        obj.from_date = (now.subtract(24, 'hour')).unix();
       } else {
-        this.type = selectedChartType
+        if (this.historyFilter.from_date) {
+          obj.from_date = (this.historyFilter.from_date.unix());
+        }
+        if (this.historyFilter.to_date) {
+          obj.to_date = this.historyFilter.to_date.unix();
+        }
       }
-    }
-    this.selectedChartType = this.type
+      obj.message_props = '';
+      let type = layoutJson ? layoutJson.chartType : this.type;
+      if (type.indexOf("Pie") >= 0) {
+        obj.message_props += currentHistoryFilter.xAxisProps + ','
+      }
+      else {
+        obj.y1AxisProperty.forEach(prop => obj.message_props += prop + ',');
+        if (obj.y2AxisProperty.length > 0) {
+          obj.y2AxisProperty.forEach(prop => obj.message_props += prop + ',');
+        }
+      }
+      if (obj.message_props.charAt(obj.message_props.length - 1) === ',') {
+        obj.message_props = obj.message_props.substring(0, obj.message_props.length - 1);
+      }
+      if (layoutJson == null || this.isLayout) {
+        obj['count'] = 10
+        delete obj.to_date
+        delete obj.from_date
+      }
+      delete obj.dateOption;
+      delete obj.y1AxisProperty;
+      delete obj.y2AxisProperty;
+      delete obj.xAxisProps
+      this.apiSubscriptions.push(this.deviceService.getDeviceTelemetry(obj).subscribe(
+        (response: any) => {
+          this.isFilterSelected = true;
+          if (response && response.data) {
+            lineGoogleChartData.dataTable = [];
+            let historyData = []
+            historyData = response.data;
+            this.historyData = historyData
+            this.isLoading = false;
+            historyData.reverse();
+
+            const dataList = [];
+            dataList.push(currentHistoryFilter.xAxisProps);
+            let title = '';
+            console.log('currentHistoryFilter.y1AxisProperty ',currentHistoryFilter.y1AxisProperty)
+            currentHistoryFilter.y1AxisProperty.forEach((prop, index) => {
+              dataList.splice(dataList.length, 0, { label: prop, type: 'number' });
+              title += prop + (index !== currentHistoryFilter.y1AxisProperty.length - 1 ? ' & ' : '');
+              lineGoogleChartData.options.series[index.toString()] = { targetAxisIndex: 0 };
+            });
+            lineGoogleChartData.options.vAxes = {
+              0: { title }
+            };
+            if (currentHistoryFilter.y2AxisProperty) {
+              title = '';
+              currentHistoryFilter.y2AxisProperty.forEach((prop, index) => {
+                dataList.splice(dataList.length, 0, { label: prop, type: 'number' });
+                title += prop + (index !== currentHistoryFilter.y2AxisProperty.length - 1 ? ' & ' : '');
+                lineGoogleChartData.options.series[(currentHistoryFilter.y1AxisProperty.length) + index] = { targetAxisIndex: 1 };
+              });
+              lineGoogleChartData.options.vAxes['1'] = { title };
+            }
+            lineGoogleChartData.dataTable.push(dataList);
+            let tempData = {}
+            if (type.indexOf('Pie') >= 0) {
+              historyData.forEach(history => {
+                if (history[currentHistoryFilter.xAxisProps]) {
+                  if (tempData[history[currentHistoryFilter.xAxisProps]]) {
+                    tempData[history[currentHistoryFilter.xAxisProps]]++
+                  }
+                  else {
+                    tempData[history[currentHistoryFilter.xAxisProps]] = 1
+                  }
+                }
+              })
+            }
+            historyData.forEach(history => {
+              history.local_created_date = this.commonService.convertUTCDateToLocal(history.message_date);
+              const list = [];
+              if (type.indexOf("Pie") >= 0) {
+                if (list.indexOf([history[currentHistoryFilter.xAxisProps], null]) <= -1) {
+                  list.splice(0, 0, history[currentHistoryFilter.xAxisProps]);
+                }
+              }
+              else {
+                list.splice(0, 0, new Date(history.local_created_date));
+                currentHistoryFilter.y1AxisProperty.forEach(prop => {
+                  if (!isNaN(parseFloat(history[prop]))) {
+                    list.splice(list.length, 0, parseFloat(history[prop]));
+                  }
+                  else {
+                    list.splice(list.length, 0, null);
+                  }
+                });
+                if (currentHistoryFilter.y2AxisProperty) {
+                  currentHistoryFilter.y2AxisProperty.forEach(prop => {
+                    if (!isNaN(parseFloat(history[prop]))) {
+                      list.splice(list.length, 0, parseFloat(history[prop]));
+                    }
+                    else {
+                      list.splice(list.length, 0, null);
+                    }
+                  });
+                }
+              }
+              lineGoogleChartData.dataTable.splice(lineGoogleChartData.dataTable.length, 0, list);
+            });
+            if (type.indexOf("Pie") >= 0) {
+              lineGoogleChartData.dataTable = []
+              lineGoogleChartData.dataTable.push([currentHistoryFilter.xAxisProps, { label: "Count", type: "number" }]);
+              let keys = Object.keys(tempData)
+              keys.forEach((key) => {
+                lineGoogleChartData.dataTable.push([key, tempData[key]])
+              })
+            }
+            lineGoogleChartData.chartType = type
+            console.log('chart ',lineGoogleChartData)
+            resolve(lineGoogleChartData)
+          }
+        }, error => this.isLoading = false
+      ));
+    })
   }
 }

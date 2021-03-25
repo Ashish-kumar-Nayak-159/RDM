72d27cd56635d9b7f286d1b72f87da05b6b90916
Urvisha Seladiya <urvisha13.seladiya@gmail.com>
2021-03-25 12:24:23 +0530

    / appended in blob url in sopan and kirloskar environment file

--- src/environments/environment-kirloskar.ts
+++ src/environments/environment-kirloskar.ts
@@ -4,7 +4,7 @@ export const environment = {
   blobAccountName: 'cmsblobstorageaccount',
   blobContainerName: 'ui-media',
   blobKey: '?sv=2019-12-12&ss=bf&srt=co&sp=rwlac&se=2049-12-31T18:30:00Z&st=2020-12-31T18:30:00Z&spr=https,http&sig=oddGuC1HeJ4b%2F3kCF2cBzUuuOWqBjwg1R0RwAshbb1o%3D',
-  blobURL: 'https://cmsblobstorageaccount.blob.core.windows.net',
+  blobURL: 'https://cmsblobstorageaccount.blob.core.windows.net/',
   cachedTelemetryContainer: 'telemetry',
   app: 'KCMS',
   version: '1.0'

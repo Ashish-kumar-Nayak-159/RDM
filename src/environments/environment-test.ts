59f12c2810e0382946d6c89d6e153e7397e0c29f
Urvisha Seladiya <urvisha13.seladiya@gmail.com>
2021-06-21 14:25:55 +0530

    release version changes

--- src/environments/environment-test.ts
+++ src/environments/environment-test.ts
@@ -8,7 +8,7 @@ export const environment = {
   cachedTelemetryContainer: 'telemetry',
   app: undefined,
   environment: 'TEST',
-  version: '8.0.1',
+  version: '8.0.2',
   storgageSecretKey: 'kEm$Y$*RdM',
   packageManagementContainer: 'packages'
 };

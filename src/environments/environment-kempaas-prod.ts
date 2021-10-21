33f8ff80367635fcfaaaae4f80bcee2453e2a784
Urvisha Seladiya <urvisha13.seladiya@gmail.com>
2021-10-21 11:43:36 +0530

    environment specific file created, package.json command added for multiple builds using signle command

---
+++ src/environments/environment-kempaas-prod.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: true,
+  appServerURL: 'https://api.kempaas.io/api/',
+  blobAccountName: 'cmsblobstorageaccount',
+  blobContainerName: 'ui-media',
+  blobKey:
+    '?sv=2019-12-12&ss=bf&srt=co&sp=rwlac&se=2049-12-31T18:30:00Z&st=2020-12-31T18:30:00Z&spr=https,http&sig=oddGuC1HeJ4b%2F3kCF2cBzUuuOWqBjwg1R0RwAshbb1o%3D',
+  blobURL: 'https://cmsblobstorageaccount.blob.core.windows.net/',
+  cachedTelemetryContainer: 'telemetry',
+  app: undefined,
+  environment: 'PROD',
+  version: '11.0',
+  packageManagementContainer: 'packages',
+  storgageSecretKey: '$0p@Ncm$',
+};

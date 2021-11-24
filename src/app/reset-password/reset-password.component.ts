e5d2d9be84844200ae640c6ee6f6e4e865a6213c
Urvisha Seladiya <urvisha13.seladiya@gmail.com>
2021-11-24 15:38:05 +0530

    User should notified on screen after the reset password (first time login) currently,it is redirected to login

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -101,9 +101,9 @@ export class ResetPasswordComponent implements OnInit, OnDestroy {
           this.changePasswordAPILoading = false;
           this.toasterService.showSuccess(response.message, 'Change Password');
           this.onModalClose();
-          if (this.isFirstTimeLogin) {
-            location.reload();
-          }
+          // if (this.isFirstTimeLogin) {
+          //   location.reload();
+          // }
           this.resetPasswordForm.reset();
         },
         (error: HttpErrorResponse) => {

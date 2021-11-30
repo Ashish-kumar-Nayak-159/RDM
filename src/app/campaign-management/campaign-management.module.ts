fd79afe90a1d14dc0e1f3f49d16e1edf9ae2688e
dj.patil <dipak.patil@kemsys.com>
2021-11-30 14:27:11 +0530

    166-Added View campaign featuer in campaign list.

--- src/app/campaign-management/campaign-management.module.ts
+++ src/app/campaign-management/campaign-management.module.ts
@@ -5,11 +5,12 @@ import { CommonModule } from '@angular/common';
 import { CampaignManagementRoutingModule } from './campaign-management-routing.module';
 import { CampaignManagementListComponent } from './campaign-management-list/campaign-management-list.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { CommonCustomModule } from '../common/common.module';
 import { Daterangepicker } from 'ng2-daterangepicker';
 import { AddCampaignComponent } from './add-campaign/add-campaign.component';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { UiSwitchModule } from 'ngx-ui-switch';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { CommonCustomModule } from '../common/common.module';
 
 @NgModule({
   declarations: [CampaignManagementListComponent, AddCampaignComponent],
@@ -18,11 +19,12 @@ import { UiSwitchModule } from 'ngx-ui-switch';
     CampaignManagementRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    AccordionModule.forRoot(),
     CommonCustomModule,
     TooltipModule,
     Daterangepicker,
     NgSelectModule,
-    UiSwitchModule,
+    UiSwitchModule    
   ],
 })
 export class CampaignManagementModule {}

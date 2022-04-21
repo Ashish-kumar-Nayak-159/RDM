bf86138d7d462fe586f1de38feb4fb21a72c6e43
Jhonny Panchal <jhonny.panchal@kemsys.com>
2022-04-21 18:44:50 +0530

    Zoho K-1-I49, K-1-I57,  and Some Enhacement

---
+++ src/app/app-maintenance/app-maintenance/app-maintenance.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppMaintenanceRoutingModule } from './app-maintenance-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppMaintenanceRoutingModule
+  ]
+})
+export class AppMaintenanceModule { }

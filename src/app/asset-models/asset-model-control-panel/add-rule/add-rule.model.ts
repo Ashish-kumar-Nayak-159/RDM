0e46008f1e1ed50fafe63b373ab99101f3d95cf9
unknown <pinki.gajjar@kemsys.com>
2022-05-18 12:51:48 +0530

    Rules Change

--- src/app/asset-models/asset-model-control-panel/add-rule/add-rule.model.ts
+++ src/app/asset-models/asset-model-control-panel/add-rule/add-rule.model.ts
@@ -14,13 +14,15 @@ export class Rule {
     aggregation_window_in_sec: any;
     updated_by: string;
     created_by: string;
-    rule_type?: boolean;
+    rules_type?: boolean;
     rule_code?: any;
     metadata?: any;
     condition?: any;
     type?: string;
     actions?: any;
-
+    category_type?:boolean;
+    rule_category?:any;
+    rule_type?:any;
     constructor() {
         this.properties = [];
         this.conditions = [];

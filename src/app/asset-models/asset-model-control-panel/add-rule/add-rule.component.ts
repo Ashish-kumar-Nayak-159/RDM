2279eea264f252ecf79ddd1d415be6b051a47ec1
unknown <pinki.gajjar@kemsys.com>
2022-05-19 16:32:56 +0530

    Asset Drop Down change

--- src/app/asset-models/asset-model-control-panel/add-rule/add-rule.component.ts
+++ src/app/asset-models/asset-model-control-panel/add-rule/add-rule.component.ts
@@ -283,7 +283,7 @@ export class AddRuleComponent implements OnInit {
       //   : [];
       if((this.ruleModel.category_type===undefined) || this.ruleModel.rules_type!==undefined && !this.ruleModel.rules_type)
       { 
-        if(this.ruleModel.rules_type)
+        if(this.ruleModel.rules_type )
         {
           response.properties.edge_derived_properties.forEach((prop) => {
             prop.type = 'Edge Derived Properties';
@@ -291,7 +291,7 @@ export class AddRuleComponent implements OnInit {
          });
         }  
       }
-      if((this.ruleModel.category_type===undefined && this.ruleModel.rules_type==undefined))
+      if((this.ruleModel.category_type===undefined && this.ruleModel.rules_type===undefined))
       {
         response.properties.edge_derived_properties.forEach((prop) => {
           prop.type = 'Edge Derived Properties';
@@ -302,14 +302,17 @@ export class AddRuleComponent implements OnInit {
       if(this.ruleModel.category_type!==undefined && !this.ruleModel.category_type && (
         this.ruleModel.rules_type==undefined || !this.ruleModel.rules_type))
       {
+        if((this.ruleModel.category_type!==undefined && this.ruleModel.rules_type!==undefined && !this.ruleModel.category_type))
+        {
         response.properties.edge_derived_properties.forEach((prop) => {
           prop.type = 'Edge Derived Properties';
           this.propertyList.push(prop);
-       });
+        });
         response.properties?.cloud_derived_properties?.forEach((prop) => {
            prop.type = 'Cloud Derived Properties';
            this.propertyList.push(prop);
         });
+       }
       }
       // response.properties.cloud_derived_properties.forEach((prop) => {
       //   prop.type = 'Cloud Derived Properties';

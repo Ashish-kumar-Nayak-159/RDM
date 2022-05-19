eb44b98177cf3a267a14bd3c47bc3b352dd51360
Jhonny Panchal <jhonny.panchal@kemsys.com>
2022-05-19 15:47:47 +0530

    Implemented the No Internet
    Implemented the No Internet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { CONSTANTS } from 'src/app/constants/app.constants';
-import { fromEvent, Observable, Subscription } from 'rxjs';
+import { fromEvent, Observable, Subscription, merge, of } from 'rxjs';
 import { Component, Inject, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
 import { Router, NavigationEnd, NavigationCancel, NavigationError, NavigationStart } from '@angular/router';
 import { DOCUMENT } from '@angular/common';
 import { CommonService } from 'src/app/services/common.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { map } from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-root',
@@ -11,6 +15,7 @@ import { CommonService } from 'src/app/services/common.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit, OnDestroy {
+
   title = 'RDM';
   isLoginRoute = false;
   isHomeRoute = false;
@@ -23,18 +28,32 @@ export class AppComponent implements OnInit, OnDestroy {
   onlineEvent: Observable<Event>;
   offlineEvent: Observable<Event>;
   subscriptions: Subscription[] = [];
+  connectionStatusMessage: string;
+  connectionStatus:string;
+  thumbnail: any;
+
+ 
+
 
   constructor(
     private router: Router,
     private commonService: CommonService,
+    private sanitizer: DomSanitizer,
     @Inject(DOCUMENT) private document: Document
   ) {
-
-  
+    this.onlineEvent = fromEvent(window, 'online');
+    this.offlineEvent = fromEvent(window, 'offline');
   }
 
   ngOnInit(): void {
-  
+    var online = navigator.onLine;
+    if(online == false){
+      this.connectionStatus = 'offline';
+    }else{
+      this.connectionStatus = 'online'
+    }
+    this.onlineEvent = fromEvent(window, 'online');
+    this.offlineEvent = fromEvent(window, 'offline');
     this.userData = this.commonService.getItemFromLocalStorage(CONSTANTS.USER_DETAILS);
     this.applicationData = this.commonService.getItemFromLocalStorage(CONSTANTS.SELECTED_APP_DATA);
     this.url = this.router.url;
@@ -43,7 +62,7 @@ export class AppComponent implements OnInit, OnDestroy {
         this.navigationInterceptor(event);
         if (event instanceof NavigationEnd) {
           this.url = event.url;
-          if (event.url.includes('login')) {
+         if (event.url.includes('login')) {
             this.isLoginRoute = true;
             this.isHomeRoute = false;
             if (!this.document.body.classList.contains('bg-white')) {
@@ -80,17 +99,55 @@ export class AppComponent implements OnInit, OnDestroy {
     );
   }
 
+
   navigationInterceptor(event): void {
-    if (event instanceof NavigationStart) {
+    var online = navigator.onLine;
+    console.log("Checking", online)
+    if(online == false){
+      this.connectionStatus = 'offline';
+    }else{
+      this.connectionStatus = 'online'
+    }
+    this.subscriptions.push(this.onlineEvent.subscribe(e => {
+      this.connectionStatusMessage = 'Back to online';
+      this.connectionStatus = 'online';
+      if(this.isHomeRoute === false){
+        if ($('#routeWrapperDiv').addClass('sb-notoggle')) {
+        }
+      }else if(this.isLoginRoute === true){
+        if ($('#routeWrapperDiv').addClass('sb-notoggle')) {
+        }
+      }else{
+        if ($('#routeWrapperDiv').removeClass('sb-notoggle')) {
+        }
+      }
+      // if(this.isLoginRoute || this.isHomeRoute){
+      //   if ($('#routeWrapperDiv').addClass('sb-notoggle')) {
+      //   }
+      // }else{
+      //   if ($('#routeWrapperDiv').removeClass('s-notoggle')) {
+      //   }
+      // }
+    }));
+
+    this.subscriptions.push(this.offlineEvent.subscribe(e => {
+      this.connectionStatusMessage = 'Connection lost! You are not connected to internet';
+      this.connectionStatus = 'offline';
+    }));
+    if (event instanceof NavigationStart && this.connectionStatus == 'online') {
       this.showLoader = true;
     }
-    if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
-      setTimeout(() => (this.showLoader = false), 500);
+    if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError || this.connectionStatus == 'online') {
+      setTimeout(() => (this.showLoader = false), 300);
     }
   }
 
+
+  
   ngOnDestroy() {
     this.apiSubscriptions.forEach((subscription) => subscription.unsubscribe());
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+
 
   }
 }

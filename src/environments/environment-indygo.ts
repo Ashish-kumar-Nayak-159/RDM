8ff7be88d56ebef54657afd9a845525b5b0549c5
jigneshprajapatiFrontEnd <jignesh.prajapati@kemsys.com>
2022-12-14 16:48:00 +0530

    change the environment version number

--- src/environments/environment-indygo.ts
+++ src/environments/environment-indygo.ts
@@ -9,7 +9,7 @@ export const environment = {
   cachedTelemetryContainer: 'telemetry',
   app: 'Indygo',
   environment: 'TEST',
-  version: '15.0.2',
+  version: '15.0.3',
   storgageSecretKey: 'kEm$Y$*RdM',
   packageManagementContainer: 'packages',
   redirectToLogin: true,

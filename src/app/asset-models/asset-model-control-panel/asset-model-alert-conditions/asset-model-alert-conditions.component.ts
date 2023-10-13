2044c489ed091238d2d8ae16414884b9df03cc62
ashish <ashishkumar.nayak@kemsys.com>
2023-10-13 14:12:33 +0530

    Remove console and comment some code which is not used in this issue

--- src/app/asset-models/asset-model-control-panel/asset-model-alert-conditions/asset-model-alert-conditions.component.ts
+++ src/app/asset-models/asset-model-control-panel/asset-model-alert-conditions/asset-model-alert-conditions.component.ts
@@ -466,7 +466,6 @@ export class AssetModelAlertConditionsComponent implements OnInit, OnDestroy {
     this.setupForm?.reset();
     if (alertObj) {
       this.alertObj = JSON.parse(JSON.stringify(alertObj));
-      console.log(this.alertObj);
       if (this.alertObj.alert_type === 'Asset') {
         this.setupForm = new FormGroup({
           slave_id: new FormControl(alertObj?.metadata?.slave_id, Validators.required),
@@ -623,16 +622,17 @@ export class AssetModelAlertConditionsComponent implements OnInit, OnDestroy {
     if(this.selectedAudioFile && this.selectedAudioFile?.name){
       await this.uploadFile();
     }
-    if(!this.alertObj?.metadata?.critical_alert_sound){
-      let modelAlert = this.commonService.getItemFromLocalStorage(CONSTANTS.MODEL_ALERT_AUDIO);
-      if(modelAlert?.length>0){
-        modelAlert.forEach( (alertAudio) => {
-          if(alertAudio?.msgCode === this.alertObj?.code && alertAudio?.alert_Id === this.alertObj?.id ){
-            console.log('code =', alertAudio?.msgCode);
-          }
-        })
-      }
-    }
+    // if(!this.alertObj?.metadata?.critical_alert_sound){
+    //   let modelAlert = this.commonService.getItemFromLocalStorage(CONSTANTS.MODEL_ALERT_AUDIO);
+    //   if(modelAlert?.length>0){
+    //     modelAlert.forEach( (alertAudio) => {
+    //       if(alertAudio?.msgCode === this.alertObj?.code && alertAudio?.alert_Id === this.alertObj?.id ){
+    //         console.log('code =', alertAudio?.msgCode);
+    //       }
+    //     })
+    //   }
+    // }
+    // Above comment code i will use to fix K-1-I1861 issue tomorrow
     this.alertObj.metadata = {
       ...this.alertObj?.metadata,
       ...this.setupForm?.value
